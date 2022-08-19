ed814ee8381702d0eed283412a57df805d2c2ea7
Smn-20 <rusekezasp@gmail.com>
2022-08-19 21:20:05 +0200

    commit before deployment

--- BSBacked/settings.py
+++ BSBacked/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -181,3 +183,5 @@ SESSION_SAVE_EVERY_REQUEST = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
